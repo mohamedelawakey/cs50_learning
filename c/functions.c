d9951c57fbb2aae1683b95b537183fdc5ae19d13
Eng Mohammed Mostafa <163655264+mohamedelawakey@users.noreply.github.com>
2025-03-20 21:04:17 +0200

    Add files via upload
    Week 2 (C Language)

---
+++ c/functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+void repetition(int n);
+int main(void){
+    repetition(3);
+}
+
+void repetition(int n){
+    for(int i =0; i < n; i++){
+        printf("Mohamed\n");
+    }
+}
